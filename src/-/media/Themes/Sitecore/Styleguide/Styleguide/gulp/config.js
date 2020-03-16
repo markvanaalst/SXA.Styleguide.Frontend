dee41826f960c50c145740052e9c3aebd74ef4b1
Mark van Aalst <markvanaalst@gmail.com>
2020-03-16 23:24:51 +0100

    Restructured sass files

--- src/-/media/Themes/Sitecore/Styleguide/Styleguide/gulp/config.js
+++ src/-/media/Themes/Sitecore/Styleguide/Styleguide/gulp/config.js
@@ -8,7 +8,7 @@ module.exports = {
     sassSourceMap: true,
     // Please configure
     serverOptions: {
-        server: 'https://sxa.sc93.local', //need to be changed
+        server: 'http://sitecorexastyleguide_cm_1', //need to be changed
         removeScriptPath: '/-/script/v2/master/RemoveMedia',
         uploadScriptPath: '/sitecore modules/PowerShell/Services/RemoteScriptCall.ashx',
         updateTemplatePath: '/-/script/v2/master/ChangeTemplate',
